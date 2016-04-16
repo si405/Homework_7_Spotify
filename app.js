840b70d80e64533fd648a25f1a38a891c7d25de1
simon-ibh <simon@icebreakerhealth.com>
2016-04-16 15:49:58 -0700

    Basic Spotify login working

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+        // Look for returned hash token from Spotify
+        // Regex to find only the access token from the URL
+        var tokenMatches = window.location.hash.match(/access_token=(.*)&token_type=*/);
+
+        if (tokenMatches) {
+
+            var accessToken = tokenMatches[1];
+
+            // Store the values in session storage that will be cleared when the browser closes
+            // Local storage would persist when the browser closes, which we don't want
+
+            window.sessionStorage.setItem("spotify_access_token", accessToken);
+
+            alert("Token stored!")
+        };
+
         // Declare variables to hold the artist and song information
 
         var artistName = '';
@@ -15,7 +31,7 @@ $(document).ready(function() {
         $("#search-form").on("submit", function(event) {
               event.preventDefault();
               var searchSong = encodeURIComponent($('#search-field').val())
-              console.log(searchSong);
+              //console.log(searchSong);
               searchURL = "https://api.spotify.com/v1/search?q=" + searchSong + "&type=track";
               $.ajax({
                   type: "GET",
@@ -24,16 +40,13 @@ $(document).ready(function() {
                       // The results are returned as an object containing tracks
                       // and an array of items. Iterate through this array to
                       // display the details
-                      console.log(results.tracks.items);
                       results.tracks.items.forEach (function(track) {
-                            console.log(track);
                             songDetails = {};
                             songDetails.songName = track.name;
                             songDetails.songPreview = track.preview_url;
                             songDetails.artistName = track.artists[0].name;
                             songDetails.albumName = track.album.name;
                             songDetails.songImage = track.album.images[0].url;
-                            console.log(songDetails);
                             var newHTML = songTemplate(songDetails);
                             $('#song-container').append(newHTML);
                       // end of results.tracks.item loop
@@ -46,6 +59,39 @@ $(document).ready(function() {
               });
           // end of search-form on submit
           });
+
+          // Login to Spotify
+
+
+          // $("#spotify-login").on('click', function () {
+          //     // Request authorization from Spotify
+          //     var client_id = '460670788eaf43a8b9d871386bbb719e';
+          //     var redirect_uri = 'http://localhost:3000/';
+          //     var scope = 'user-library-read user-library-modify';
+          //     var url = 'https://accounts.spotify.com/authorize';
+          //     url += '?response_type=token';
+          //     url += '&client_id=' + encodeURIComponent(client_id);
+          //     url += '&scope=' + encodeURIComponent(scope);
+          //     url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+          //     window.location = url;
+          //       $.ajax({
+          //           type: "GET",
+          //           url: "https://accounts.spotify.com/authorize?client_id=460670788eaf43a8b9d871386bbb719e&response_type=token&redirect_uri=http://localhost:3000&scope=user_library_read user_library_modify",
+          //           success: function(results) {
+          //               console.log(results);
+          //                   alert("Stored token!");
+          //           },
+          //           error: function(request, status, error) {
+          //               console.log(request);
+          //               console.log(error);
+          //               alert("There was an error authorizing with Spotify");
+          //           }
+          //
+          //       // end of Ajax call
+          //       });
+          // // end of #spotify-login on click
+          // });
+
 
 // End of document ready
 });
